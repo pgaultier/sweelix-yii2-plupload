44020ff3a0631e3eb1483387f3f9ad579d3b6ab4
Philippe Gaultier <pgaultier@sweelix.net>
2014-03-26 20:12:05 +0100

    Adding better asset management

---
+++ src/PluploadUiAsset.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * File PluploadUiAsset.php
+ *
+ * PHP version 5.4+
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2014 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   XXX
+ * @link      http://www.sweelix.net
+ * @category  plupload
+ * @package   sweelix.yii2.plupload
+ */
+
+namespace sweelix\yii2\plupload;
+use yii\web\AssetBundle;
+
+/**
+ * Class PluploadOriginalAsset embed plupload script files
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2014 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   XXX
+ * @link      http://www.sweelix.net
+ * @category  plupload
+ * @package   sweelix.yii2.plupload
+ * @since     XXX
+ */
+class PluploadUiAsset extends AssetBundle {
+	public $sourcePath = '@sweelix/yii2/plupload/assets';
+	public $js = [
+		'sweelix.plupload.ui.js',
+	];
+	public $css = [
+		'sweelix.plupload.ui.css',
+	];
+    public $depends = [
+        'sweelix\yii2\plupload\PluploadAsset',
+    ];
+}
